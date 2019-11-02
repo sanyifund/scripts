9c6ed101cbb6e6f7e55d8b155276f4dfb5a0a41a
Liaune <1181382547@qq.com>
2019-11-02 11:13:47 +0800

    Update Tinygrail_Collect_My_Auction.js

--- liaune/Tinygrail_Collect_My_Auction.js
+++ liaune/Tinygrail_Collect_My_Auction.js
@@ -22,9 +22,10 @@ function postData(url, data, callback) {
     success: callback
   });
 }
+var username = $('#dock .clearit li.first a')[0].innerText;
 getData(`chara/user/auction/1/1000`,function (d, s) {
 	var chara = {};
-	document.write('<table><tbody><tr><th>ID</th><th>角色</th><th>出价</th><th>数量</th></tr></tbody><tbody>');
+	document.write('<table><tbody><tr><th>User</th><th>ID</th><th>角色</th><th>出价</th><th>数量</th></tr></tbody><tbody>');
 	for(i=0;i<d.Value.Items.length; i++){
 		var Id = d.Value.Items[i].CharacterId;
 		var State = d.Value.Items[i].State;
@@ -34,7 +35,7 @@ getData(`chara/user/auction/1/1000`,function (d, s) {
 		chara[Id].Price = d.Value.Items[i].Price;
 		chara[Id].Amount = d.Value.Items[i].Amount;
 		if(chara[Id].Price && chara[Id].Amount && State==0)
-		document.write('<tr><td><a href="https://bgm.tv/character/'+Id+'" class="l">'+Id+'</a></td><td>'+chara[Id].Name+'</td><td>'+chara[Id].Price+'</td><td>'+chara[Id].Amount+'</td></tr>');
+		document.write('<tr><td>'+username+'<td><a href="https://bgm.tv/character/'+Id+'" class="l">'+Id+'</a></td><td>'+chara[Id].Name+'</td><td>'+chara[Id].Price+'</td><td>'+chara[Id].Amount+'</td></tr>');
 	}
 	//console.table(chara);
 });
